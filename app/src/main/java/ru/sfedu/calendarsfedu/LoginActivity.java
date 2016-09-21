7a51bdfa23e8d0c83743e157e90a8d02cd9dfcaa
Leva <leva1824@gmail.com>
2016-09-21 13:23:02 +0300

    Работаййййй

--- app/src/main/java/ru/sfedu/calendarsfedu/LoginActivity.java
+++ app/src/main/java/ru/sfedu/calendarsfedu/LoginActivity.java
@@ -42,7 +42,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     /**
      * Id to identity READ_CONTACTS permission request.
      */
-    private static final int REQUEST_READ_CONTACTS = 0;
+    private static final int REQUEST_READ_CONTACTS = 1;
 
     /**
      * A dummy authentication store containing known user names and passwords.
